016a0a0542a3b355a8383e918c96659ee48041ad
HenriVIII1509 <62750489+HenriVIII1509@users.noreply.github.com>
2023-12-31 18:13:05 +0000

    Package refactoring

--- src/App.js
+++ src/App.js
@@ -7,17 +7,17 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          My first changes to the react app
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
+      <body>
+        <ul>
+          <li><a class="active" href="#home">Home</a></li>
+          <li><a href="#news">News</a></li>
+          <li><a href="#contact">Contact</a></li>
+          <li><a href="#about">About</a></li>
+        </ul>
+      </body>
     </div>
   );
 }
